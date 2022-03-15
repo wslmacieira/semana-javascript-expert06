81d75877c13b32e4ce17e5351ca1a7944c4b7533
wslmacieira <wslmacieira@outlook.com>
2022-03-15 00:05:52 -0300

    planejando os testes para camada de rotas

--- tests/unit/server/routes.test.js
+++ tests/unit/server/routes.test.js
@@ -1,7 +1,25 @@
-import { jest, expect, describe, test } from '@jest/globals';
+import { jest, expect, describe, test, beforeEach } from '@jest/globals';
+import config from '../../../server/config.js'
 
-describe('#Routes', () => {
-  test('my test', () => {
-    expect(true).toBeTruthy()
+const {
+  pages
+} = config
+
+
+describe('#Routes - test site for api response', () => {
+  beforeEach(() => {
+    jest.restoreAllMocks()
+    jest.clearAllMocks()
   })
-})
+
+  test.todo('GET / - should redirect to home page')
+  test.todo(`GET /home - should response with ${pages.homeHTML} file stream`)
+  test.todo(`GET /controller - should response with ${pages.controllerHTML} file stream`)
+  test.todo(`GET /file.ext - should response with file stream`)
+  test.todo(`GET /unknown - give an inexistent route it should response with 404`)
+
+  describe('exceptions', () => {
+    test.todo('given inexistent file it should respond with 404')
+    test.todo('given an error file it should respond with 500')
+  })
+});
