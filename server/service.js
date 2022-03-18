cc6d675b0092f346351ddeef9eb9bc2c5c91d774
wslmacieira <wslmacieira@outlook.com>
2022-03-18 18:55:58 -0300

    inicializando streams para mandar  o audio para o client

--- server/service.js
+++ server/service.js
@@ -3,15 +3,31 @@ import { join, extname } from 'path';
 import config from './config.js';
 import fsPromises from 'fs/promises';
 import { randomUUID } from 'crypto';
-import { PassThrough } from 'stream';
+import { PassThrough, Writable } from 'stream';
+import { once } from  'events';
+import streamsPromises from 'stream/promises';
+import Throttle from 'throttle';
+import childProcess from 'child_process';
+import { logger } from './util.js'
 
 const {
   dir: { publicDirectory },
+  constants: {
+    fallbackBitRate,
+    englishConversation,
+    bitRateDivisor
+  }
 } = config;
 
 export class Service {
   constructor() {
     this.clientStreams = new Map();
+    this.currentSong = englishConversation;
+    this.currentBitRate = 0;
+    this.throttleTransform = {}
+    this.currentReadable = {}
+
+    this.startStreaming()
   }
 
   createClientStream() {
@@ -28,6 +44,71 @@ export class Service {
   removeClientStream(id) {
     this.clientStreams.delete(id);
   }
+
+  _executeSoxCommand(args) {
+    return childProcess.spawn('sox', args)
+  }
+
+  async getBitRate(song) {
+    try {
+      const args = [
+        "--i", // info
+        "-B", // bitrate
+        song
+      ]
+      const {
+        stderr, // tudo que é erro
+        stdout, // tudo que é log
+        // stdin // enviar dados como stream
+      } = this._executeSoxCommand(args)
+      await Promise.all([
+        once(stdout, 'readable'),
+        once(stderr, 'readable')
+      ])
+
+      const [success, error] = [stdout, stderr].map(stream => stream.read())
+      if (error) {
+        return await Promise.reject(error)
+      }
+
+      return success
+      .toString()
+      .trim()
+      .replace(/k/, '000')
+    } catch (error) {
+      logger.error(`deu ruim no bitrate: ${error}`)
+      return fallbackBitRate
+    }
+  }
+
+  broadCast() {
+    return new Writable({
+      write: (chunk, enc, cb) => {
+        for (const [id, stream] of this.clientStreams) {
+          // se o cliente desconectou não devemos mais mandar dados para ele
+          if(stream.writableEnded) {
+            this.clientStreams.delete(id);
+            continue;
+          }
+          stream.write(chunk);
+        }
+        cb()
+      }
+    })
+  }
+
+  async startStreaming() {
+    logger.info(`starting with ${this.currentSong}`);
+    const bitRate = this.currentBitRate = await this.getBitRate(this.currentSong) / bitRateDivisor;
+    const throttleTransform = this.throttleTransform =  new Throttle(bitRate);
+    const songReadable = this.currentReadable =  this.createFileStream(this.currentSong)
+    streamsPromises.pipeline(
+      songReadable,
+      throttleTransform,
+      this.broadCast()
+    )
+  }
+
   createFileStream(filename) {
     return fs.createReadStream(filename);
   }
