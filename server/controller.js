a2b518b098b6d6f6ed96713ef3ff92d85e7e4f10
wslmacieira <wslmacieira@outlook.com>
2022-03-18 17:31:24 -0300

    criando o createClientStream na controller

--- server/controller.js
+++ server/controller.js
@@ -1,4 +1,5 @@
 import { Service } from './service.js';
+import { logger } from './util.js'
 
 export class Controller {
   constructor() {
@@ -7,5 +8,22 @@ export class Controller {
 
   async getFileStream(filename) {
     return this.service.getFileStream(filename);
+  }
+
+  createClientStream() {
+    const {
+      id,
+      clientStream
+    } = this.service.createClientStream()
+
+    const onClose = () => {
+      logger.info(`closing connection of ${id}`)
+      this.service.removeClientStream(id)
+    }
+
+    return {
+      stream: clientStream,
+      onClose
+    }
   }
 }
